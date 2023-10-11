dcd87ee24674f0e59af8b1391817f86708171748
Ji Seungmin <cswcsm02@gmail.com>
2023-10-11 23:52:49 +0900

    [feat]: 로그인 및 JWT 토큰을 통해 회원 조회 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,10 @@
 import { LoginRequestDto } from 'src/auth/dto/login.request.dto';
 import { CreateUserDto } from './dto/user.create.dto';
 import { UserService } from './user.service';
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from 'src/auth/auth.service';
+import { CurrentUser } from 'src/common/decorator/user.decorator';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.guard';
 
 @Controller('user')
 export class UserController {
@@ -11,9 +13,15 @@ export class UserController {
     private readonly authService: AuthService,
   ) {}
 
-  @Get()
+  @Get('/list')
   async findAll() {
     return await this.userService.findAll();
+  }
+
+  @Get()
+  @UseGuards(JwtAuthGuard)
+  async findUser(@CurrentUser() user) {
+    return user;
   }
 
   @Post()
