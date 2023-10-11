2141d0de143dc9e7e7f20d4dd973576e784ea54a
Ji Seungmin <cswcsm02@gmail.com>
2023-10-11 23:48:15 +0900

    [feat]: 로그인시 jwt 토큰 발급 기능 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,11 @@ import { JwtStrategy } from './jwt/jwt.strategy';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     PassportModule.register({ defaultStrategy: 'jwt', session: false }),
 
     JwtModule.register({
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,15 @@
+import { LoginRequestDto } from 'src/auth/dto/login.request.dto';
 import { CreateUserDto } from './dto/user.create.dto';
 import { UserService } from './user.service';
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Get()
   async findAll() {
@@ -14,5 +19,10 @@ export class UserController {
   @Post()
   create(@Body() userDto: CreateUserDto) {
     return this.userService.save(userDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginDto: LoginRequestDto) {
+    return this.authService.jwtLogin(loginDto);
   }
 }
